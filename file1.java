45ef0db99542bb6d3d438734eb790d53cc2c7fef
David <davidxpatel123@gmail.com>
2026-07-27 21:38:52 +0530

    Master branch file1 java

--- file1.java
+++ file1.java
@@ -1,16 +1,1 @@
-import java.util.*;
-import java.util.Queue;
-import java.time.*;
-import java.lang.*;
-import java.io.*;
-//this is just a sample file 
-public class file1
-{
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("File-1 work starts....");
-        System.out.println("Some extra changes are made here");
-        sc.close();
-    }
-}
+master branch 
