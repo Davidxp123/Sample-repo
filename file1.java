9cda6ecaf560c26c1852944076aa6d3242c24083
David <davidxpatel123@gmail.com>
2026-07-27 14:02:17 +0530

    Adding readme and file1 java files

---
+++ file1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Queue;
+import java.time.*;
+import java.lang.*;
+import java.io.*;
+
+public class file1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("File-1 work starts....");
+        
+        sc.close();
+    }
+}
